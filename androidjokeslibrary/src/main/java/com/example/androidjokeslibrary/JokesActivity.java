44f76d2173e1f150263728644d932d21fd6abcea
Pradeep <krnprdp@gmail.com>
2016-04-28 14:25:08 -0400

    Modify AndroidjokesLibrary

--- androidjokeslibrary/src/main/java/com/example/androidjokeslibrary/JokesActivity.java
+++ androidjokeslibrary/src/main/java/com/example/androidjokeslibrary/JokesActivity.java
@@ -1,16 +1,19 @@
 package com.example.androidjokeslibrary;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * Created by Pradeep on 4/28/16.
+ */
+public class JokesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.jokes_layout);
         Intent intent = getIntent();
         String joke = intent.getStringExtra(Intent.EXTRA_TEXT);
         TextView tv = (TextView) findViewById(R.id.textView);
