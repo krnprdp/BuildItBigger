c5bac1a2e004496a6aa57244310346e6f7c47a4f
Pradeep <krnprdp@gmail.com>
2016-04-28 14:37:20 -0400

    feat: GCE getJoke() from java library

---
+++ backend/src/main/java/com/example/Pradeep/myapplication/backend/GetJokesBean.java
@@ -0,0 +1,17 @@
+package com.example.Pradeep.myapplication.backend;
+
+/**
+ * Created by Pradeep on 4/28/16.
+ */
+public class GetJokesBean {
+
+    private String mJoke;
+
+    public void setJoke(String joke) {
+        mJoke = joke;
+    }
+
+    public String getJoke() {
+        return mJoke;
+    }
+}
