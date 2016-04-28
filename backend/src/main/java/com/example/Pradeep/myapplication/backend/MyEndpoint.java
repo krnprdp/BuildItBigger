c1be338ea1d64faa6faed241946ce206411c54da
Pradeep <krnprdp@gmail.com>
2016-04-28 13:02:22 -0400

    modify endpoint

--- backend/src/main/java/com/example/Pradeep/myapplication/backend/MyEndpoint.java
+++ backend/src/main/java/com/example/Pradeep/myapplication/backend/MyEndpoint.java
@@ -6,30 +6,43 @@
 
 package com.example.Pradeep.myapplication.backend;
 
+import com.example.JokesContainer;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
 
 import javax.inject.Named;
 
-/** An endpoint class we are exposing */
+/**
+ * An endpoint class we are exposing
+ */
 @Api(
-  name = "myApi",
-  version = "v1",
-  namespace = @ApiNamespace(
-    ownerDomain = "backend.myapplication.Pradeep.example.com",
-    ownerName = "backend.myapplication.Pradeep.example.com",
-    packagePath=""
-  )
+        name = "myApi",
+        version = "v1",
+        namespace = @ApiNamespace(
+                ownerDomain = "backend.myapplication.Pradeep.example.com",
+                ownerName = "backend.myapplication.Pradeep.example.com",
+                packagePath = ""
+        )
 )
 public class MyEndpoint {
 
-    /** A simple endpoint method that takes a name and says Hi back */
+    /**
+     * A simple endpoint method that takes a name and says Hi back
+     */
     @ApiMethod(name = "sayHi")
     public MyBean sayHi(@Named("name") String name) {
         MyBean response = new MyBean();
         response.setData("Hi, " + name);
 
+        return response;
+    }
+
+    @ApiMethod(name = "getJoke")
+    public GetJokesBean getJoke() {
+        GetJokesBean response = new GetJokesBean();
+        JokesContainer jokesContainer = new JokesContainer();
+        response.setJoke(jokesContainer.getJoke());
         return response;
     }
 
