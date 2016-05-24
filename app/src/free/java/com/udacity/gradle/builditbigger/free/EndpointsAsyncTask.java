db8a17de9df76f5e0059938484cc5972a4bfbefa
Pradeep <krnprdp@gmail.com>
2016-05-24 12:36:50 -0400

    Added free source set to the app

---
+++ app/src/free/java/com/udacity/gradle/builditbigger/free/EndpointsAsyncTask.java
@@ -0,0 +1,83 @@
+package com.udacity.gradle.builditbigger;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.util.Pair;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.example.androidjokeslibrary.JokesActivity;
+import com.example.pradeep.myapplication.backend.myApi.MyApi;
+import com.example.pradeep.myapplication.backend.myApi.model.GetJokesBean;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+
+import java.io.IOException;
+
+/**
+ * Created by Pradeep on 4/27/16.
+ */
+class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, String> {
+    private static MyApi myApiService = null;
+    private Context context;
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected String doInBackground(Pair<Context, String>... params) {
+        if (myApiService == null) {  // Only do this once
+//            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+//                    new AndroidJsonFactory(), null)
+//                    // options for running against local devappserver
+//                    // - 10.0.2.2 is localhost's IP address in Android emulator
+//                    // - turn off compression when running against local devappserver
+//                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+//                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+//                        @Override
+//                        public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+//                            abstractGoogleClientRequest.setDisableGZipContent(true);
+//                        }
+//                    });
+            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
+                    .setRootUrl("https://splendid-tower-129416.appspot.com/_ah/api/");
+//             end options for devappserver
+
+            myApiService = builder.build();
+        }
+
+        context = params[0].first;
+        String name = params[0].second;
+
+//        try {
+//            return myApiService.sayHi(name).execute().getData();
+//        } catch (IOException e) {
+//            return e.getMessage();
+//        }
+        try {
+            return myApiService.getJoke().execute().getJoke();
+        } catch (IOException error) {
+            return error.getMessage();
+        }
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        //Toast.makeText(context, result, Toast.LENGTH_LONG).show();
+        if (result != null && result != "") {
+            Intent intent = new Intent(context, JokesActivity.class);
+            intent.putExtra(Intent.EXTRA_TEXT, result);
+            context.startActivity(intent);
+        } else {
+            Toast.makeText(context, "Network Error. Check your connection.", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
